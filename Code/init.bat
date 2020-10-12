b93361ec8f87b407b4af29e3a6a584ee4a0b1328
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-12 12:02:14 -0400

    Update init.bat

--- Code/init.bat
+++ Code/init.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 :PROMPT
-SET /P startq=Would you like to start the program now? (Y/[N])? 
+SET /P startq=Would you like to start the program now? (Y/[N]) 
 IF /I "%startq%" NEQ "Y" GOTO END
 
 run.bat
