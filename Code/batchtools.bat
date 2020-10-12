d83dbe8ab4275a63d692473a7bb98448dd1f822c
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-12 12:44:31 -0400

    Added a trash update checker
    Its bad because its not automatic yet ):

--- Code/batchtools.bat
+++ Code/batchtools.bat
@@ -3,19 +3,24 @@
 cls
 ECHO.
 ECHO 1 - Report Issue
-ECHO 2 - Soon
+ECHO 2 - Check For Updates (Currently 0.0.6)
 ECHO 3 - Soon
 ECHO 4 - Exit
 ECHO.
 SET /P MENU=Enter your option: 
 IF %MENU%==1 goto ISSUE
-IF %MENU%==2 goto SOON
+IF %MENU%==2 goto UPDATE
 IF %MENU%==3 goto SOON
 IF %MENU%==4 goto EXIT
 goto START
 
 :ISSUE
 start issue.url
+goto START
+
+:UPDATE
+start release.url
+pause
 goto START
 
 :SOON
