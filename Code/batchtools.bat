dc0a6d656eaf61fbc08f62a583172439a0abfe02
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-13 11:03:28 -0400

    Added credits

--- Code/batchtools.bat
+++ Code/batchtools.bat
@@ -3,13 +3,13 @@
 cls
 echo.
 echo 1 - Report Issue
-echo 2 - Check For Updates (Currently 0.0.8b)
+echo 2 - Check For Updates (Currently 0.0.8)
 echo 3 - Soon
 echo 4 - Soon
 echo 5 - Soon
 echo 6 - Soon
 echo 7 - Soon
-echo 8 - Soon
+echo 8 - Credits
 echo 9 - Restart Batch Tools
 echo 10 - Exit
 echo.
@@ -21,28 +21,38 @@ if %MENU%==4 goto SOON
 if %MENU%==5 goto SOON
 if %MENU%==6 goto SOON
 if %MENU%==7 goto SOON
-if %MENU%==8 goto SOON
+if %MENU%==8 goto CREDITS
 if %MENU%==9 goto RESTART
 if %MENU%==10 goto EXIT
 goto START
 
 :ISSUE
+cls
 start issue.url
 goto START
 
 :UPDATE
+cls
 start release.url
 pause
 goto START
 
 :SOON
+cls
 echo Coming Soon!
 pause
 goto START
 
+:Credits
+cls
+start credits.url
+goto START
+
 :Restart
+cls
 echo One moment
 init.bat
 goto EXIT
+
 :EXIT
 exit
