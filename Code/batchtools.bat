268f2b812c0fa6fbf0ff45d2b692c219851be255
Gimenez Killian <9Q.B7@pcsb.org>
2020-10-13 08:52:32 -0400

    haha 666 go brrrr

--- Code/batchtools.bat
+++ Code/batchtools.bat
@@ -3,15 +3,27 @@
 cls
 echo.
 echo 1 - Report Issue
-echo 2 - Check For Updates (Currently 0.0.6)
+echo 2 - Check For Updates (Currently 0.0.7b)
 echo 3 - Soon
-echo 4 - Exit
+echo 4 - Soon
+echo 5 - Soon
+echo 6 - Soon
+echo 7 - Soon
+echo 8 - Soon
+echo 9 - Soon
+echo 10 - Exit
 echo.
 set /P MENU=Enter your option: 
 if %MENU%==1 goto ISSUE
 if %MENU%==2 goto UPDATE
 if %MENU%==3 goto SOON
-if %MENU%==4 goto EXIT
+if %MENU%==4 goto SOON
+if %MENU%==5 goto SOON
+if %MENU%==6 goto SOON
+if %MENU%==7 goto SOON
+if %MENU%==8 goto SOON
+if %MENU%==9 goto SOON
+if %MENU%==10 goto EXIT
 goto START
 
 :ISSUE
